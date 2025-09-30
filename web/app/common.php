325371b0c4f5fa475174585eb362d44f3593790d
daxia <78778443@qq.com>
2025-09-30 23:57:17 +0800

    统一header头

--- web/app/common.php
+++ web/app/common.php
@@ -1,2 +1,25 @@
 <?php
+// +----------------------------------------------------------------------
+// | ThinkPHP [ WE CAN DO IT JUST THINK ]
+// +----------------------------------------------------------------------
+// | Copyright (c) 2006~2018 http://thinkphp.cn All rights reserved.
+// +----------------------------------------------------------------------
+// | Licensed ( http://www.apache.org/licenses/LICENSE-2.0 )
+// +----------------------------------------------------------------------
+// | Author: liu21st <liu21st@gmail.com>
+// +----------------------------------------------------------------------
+
+\think\facade\Config::set(['app_dispatch_on' => true], 'app');
+
 // 应用公共文件
+
+/**
+ * 渲染导航栏
+ * @param string $currentPage 当前页面标识
+ * @return string 导航栏HTML
+ */
+function renderNavbar($currentPage = '') {
+    // 传递当前页面标识到导航栏模板
+    $vars = ['page' => $currentPage];
+    return \think\facade\View::fetch('common/navbar', $vars);
+}
--- web/app/controller/Index.php
+++ web/app/controller/Index.php
@@ -1,20 +1,39 @@
 <?php
-
 namespace app\controller;
 
 use app\BaseController;
+use think\facade\View;
+use think\facade\Db;
 
 class Index extends BaseController
 {
     public function index()
     {
+        // 获取最新上传的图片
+        $latestPhotos = Db::name('vr_photos')
+            ->alias('p')
+            ->join('users u', 'p.user_id = u.id')
+            ->field('p.*, u.username')
+            ->where('p.is_public', 1)
+            ->order('p.created_at', 'desc')
+            ->limit(6)
+            ->select()
+            ->toArray();
 
-        return redirect('/photo');
+        // 获取热门图片（按浏览次数排序）
+        $popularPhotos = Db::name('vr_photos')
+            ->alias('p')
+            ->join('users u', 'p.user_id = u.id')
+            ->field('p.*, u.username')
+            ->where('p.is_public', 1)
+            ->order('p.view_count', 'desc')
+            ->limit(6)
+            ->select()
+            ->toArray();
 
-    }
-
-    public function hello($name = 'ThinkPHP8')
-    {
-        return 'hello,' . $name;
+        return View::fetch('index', [
+            'latestPhotos' => $latestPhotos,
+            'popularPhotos' => $popularPhotos
+        ]);
     }
 }
